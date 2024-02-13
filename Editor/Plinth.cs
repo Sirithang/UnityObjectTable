726930b9f42a2b445b4448c10a6fc9391c71b07e
Felipe Rodrigues <felipergr@hotmail.com>
2024-02-13 20:32:33 -0300

    Update Plinth.cs

--- Editor/Plinth.cs
+++ Editor/Plinth.cs
@@ -428,7 +428,7 @@ public class DatabaseDisplayer : TreeView
         float height = 0;
         var item = (DatabaseViewerItem)treeItem;
         for(int i=0; i<item.properties.Length; i++) {
-            float newHeight = EditorGUI.GetPropertyHeight(item.properties[i], item.properties[i].isArray && item.properties[i].isExpanded);
+            float newHeight = EditorGUI.GetPropertyHeight(item.properties[i], item.properties[i].isExpanded);
             height = Mathf.Max(height, newHeight);
         }
         return height;
